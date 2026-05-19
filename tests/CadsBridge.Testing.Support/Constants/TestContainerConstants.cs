b717ec5476d01a0480142e1279eeba815f03a294
andy-defra <andy.rose@esynergy.co.uk>
2026-05-19 16:13:00 +0100

    Adding some infrastructure tests

---
+++ tests/CadsBridge.Testing.Support/Constants/TestContainerConstants.cs
@@ -0,0 +1,11 @@
+namespace CadsBridge.Testing.Support.Constants;
+
+public static class TestContainerConstants
+{
+    // Network
+    public const string NetworkName = "integration-test-network";
+    public const string NetworkAlias = "localstack";
+
+    // Port
+    public const int LocalStackPort = 4566;
+}
