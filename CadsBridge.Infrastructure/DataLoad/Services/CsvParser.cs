d4ad2e2bcd1f5667a6a440f162ebe69a53eb2ea6
andy-defra <andy.rose@esynergy.co.uk>
2026-06-30 13:05:52 +0100

    Fixing interface parameter name

--- CadsBridge.Infrastructure/DataLoad/Services/CsvParser.cs
+++ CadsBridge.Infrastructure/DataLoad/Services/CsvParser.cs
@@ -5,12 +5,12 @@ namespace CadsBridge.Infrastructure.DataLoad.Services;
 
 public class CsvParser : ICsvParser
 {
-    public IEnumerable<string> ParseCsvLine(string csvLineContent, char delimiter = '|', int expectedCount = 0)
+    public IEnumerable<string> ParseCsvLine(string csvContent, char delimiter = '|', int expectedCount = 0)
     {
-        var parts = csvLineContent.Split('|');
+        var parts = csvContent.Split('|');
         if (expectedCount > 0 && parts.Length != expectedCount)
         {
-            throw new DomainException($"Trailer line has {parts.Length} field(s); expected 4. Line: '{csvLineContent}'");
+            throw new DomainException($"Trailer line has {parts.Length} field(s); expected 4. Line: '{csvContent}'");
         }
         return parts;
     }
