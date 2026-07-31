a0bccd242bf0f8db1bb8a4ce5e72ca90efb0ac8e
Mark Gent <MarkGent1@users.noreply.github.com>
2026-07-31 13:36:17 +0100

    LANI-729: Updated password deriving logic (#44)
    Co-authored-by: Mark Gent <Mark.Gent@homesengland.gov.uk>

--- CadsBridge.Infrastructure/DataLoad/Csv/Extensions/CtsmFilenameExtensions.cs
+++ CadsBridge.Infrastructure/DataLoad/Csv/Extensions/CtsmFilenameExtensions.cs
@@ -10,7 +10,15 @@ public static class CtsmFilenameExtensions
         {
             var timeStamp = ctsmFilename.Timestamp.Substring(0, ctsmFilename.Timestamp.LastIndexOf('-'));
             var reversedTableName = string.Join("_", ctsmFilename.TableName.Split('_').Reverse());
-            return $"{timeStamp}_{reversedTableName}_{ctsmFilename.BatchId}_{ctsmFilename.Type}_{ctsmFilename.Env}_{ctsmFilename.App}_CTSM";
+
+            if (!string.IsNullOrWhiteSpace(ctsmFilename.PartNo))
+            {
+                return $"{timeStamp}_{reversedTableName}_{ctsmFilename.PartNo}_{ctsmFilename.BatchId}_{ctsmFilename.Type}_{ctsmFilename.Env}_{ctsmFilename.App}_CTSM";
+            }
+            else
+            {
+                return $"{timeStamp}_{reversedTableName}_{ctsmFilename.BatchId}_{ctsmFilename.Type}_{ctsmFilename.Env}_{ctsmFilename.App}_CTSM";
+            }
         }
     }
 }
