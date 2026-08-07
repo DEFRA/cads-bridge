b01cda60d34d2e0e4e0a6723ead1677722f6663d
David Baillie <david.baillie@esynergy.co.uk>
2026-08-07 13:35:30 +0100

    Removed redundant file

--- compose/copy-data.bat
+++
@@ -1,6 +0,0 @@
-aws s3 cp ./data/CTSM_UKV_PROD_BULK_######_CT_EARTAG_TYPES_2026-02-22-074603.csv s3://cads-bridge-external-bucket/CTSM_UKV_PROD_BULK_######_CT_EARTAG_TYPES_2026-02-22-074603.csv --profile localstack
-aws s3 cp ./data/CTSM_UKV_PROD_BULK_######_CT_LOCATION_TYPES_2026-02-22-074603.csv s3://cads-bridge-external-bucket/CTSM_UKV_PROD_BULK_######_CT_LOCATION_TYPES_2026-02-22-074603.csv --profile localstack
-aws s3 cp ./data/CTSM_UKV_PROD_BULK_######_CT_LOCATION_RELATIONSHIPS_2026-02-22-074604.csv s3://cads-bridge-external-bucket/CTSM_UKV_PROD_BULK_######_CT_LOCATION_RELATIONSHIPS_2026-02-22-074604.csv --profile localstack
-
-aws s3 ls cads-external-bucket --profile localstack  
-aws s3 ls cads-internal-bucket --profile localstack  
