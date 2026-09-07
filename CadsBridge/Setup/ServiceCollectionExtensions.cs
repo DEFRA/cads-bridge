dfdf1e8ec2f03317442f41c7672466b0f56b1579
andy-defra <andy.rose@esynergy.co.uk>
2026-09-07 14:12:46 +0100

    Adding basic auth boilerplate set up

--- CadsBridge/Setup/ServiceCollectionExtensions.cs
+++ CadsBridge/Setup/ServiceCollectionExtensions.cs
@@ -1,15 +1,22 @@
 using CadsBridge.Application.Setup;
+using CadsBridge.Infrastructure.Authentication.Configuration;
+using CadsBridge.Infrastructure.Authentication.Handlers;
 using CadsBridge.Infrastructure.Configuration.Aws;
 using CadsBridge.Infrastructure.Json;
 using CadsBridge.Infrastructure.Setup;
 using CadsBridge.Worker.Setup;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
 
 namespace CadsBridge.Setup;
 
 public static class ServiceCollectionExtensions
 {
-    public static void ConfigureCds(this IServiceCollection services, IConfiguration configuration)
+    public static void ConfigureCadsBridge(this IServiceCollection services, IConfiguration configuration)
     {
+        services.ConfigureAuthentication(configuration);
+
         services.AddControllers()
             .AddJsonOptions(opts =>
             {
@@ -29,5 +36,35 @@ public static class ServiceCollectionExtensions
         services.AddBackgroundServiceScheduling(configuration);
 
         services.AddApplicationLayer();
+    }
+
+    private static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var authConfig = configuration.GetSection(nameof(AuthenticationConfiguration)).Get<AuthenticationConfiguration>()!;
+
+        services.Configure<AclOptions>(
+            configuration.GetSection("Acl"));
+
+        services.Configure<AuthenticationConfiguration>(
+            configuration.GetSection("AuthenticationConfiguration"));
+        var authenticationBuilder = services.AddAuthentication();
+        var authorizationBuilder = services.AddAuthorizationBuilder();
+        if (authConfig.ApiKey.Enabled)
+        {
+            AddApiKeyScheme(authenticationBuilder);
+            authorizationBuilder.AddApiKeyPolicy();
+        }
+    }
+
+    private static void AddApiKeyScheme(this AuthenticationBuilder authenticationBuilder)
+    {
+        authenticationBuilder.AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>(
+            AuthenticationConstants.ApiKeySchemeName, _ => { });
+    }
+
+    private static void AddApiKeyPolicy(this AuthorizationBuilder authorizationBuilder)
+    {
+        authorizationBuilder.AddPolicy(AuthenticationConstants.ApiKeyPolicyName,
+            policy => policy.AddAuthenticationSchemes(AuthenticationConstants.ApiKeySchemeName).RequireAuthenticatedUser());
     }
 }
--- tests/CadsBridge.Testing.Support/TestFixtures/Components/WebAppFactoryBase.cs
+++ tests/CadsBridge.Testing.Support/TestFixtures/Components/WebAppFactoryBase.cs
@@ -22,6 +22,7 @@ using Microsoft.Extensions.Hosting;
 using Moq;
 using System.Globalization;
 using System.Net;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CadsBridge.Testing.Support.TestFixtures.Components;
 
@@ -66,6 +67,7 @@ public abstract class WebAppFactoryBase<TStart>(
 
         builder.ConfigureTestServices(services =>
         {
+            ConfigureDefaultAuthorization(services);
             OverrideAmazonS3(services);
             OverrideAmazonSqs(services);
             ConfigureMessageConsumers(services);
@@ -130,6 +132,14 @@ public abstract class WebAppFactoryBase<TStart>(
 
         Environment.SetEnvironmentVariable("IMB_S3_ACCESS_KEY", "test");
         Environment.SetEnvironmentVariable("IMB_S3_ACCESS_SECRET", "test");
+    }
+
+    private static void ConfigureDefaultAuthorization(IServiceCollection services)
+    {
+        services.AddAuthorizationBuilder()
+            .SetDefaultPolicy(new AuthorizationPolicyBuilder()
+                .RequireAssertion(_ => true)
+                .Build());
     }
 
     private void ResetInfrastructureMocks()
